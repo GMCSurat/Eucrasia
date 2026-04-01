dcb6632b582cc3725e17f95ce76b83d14c6ab51a
Prathameshppawar08 <prathameshppawar08@gmail.com>
2026-04-01 16:07:47 +0530

    width change done

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -25,7 +25,7 @@ export default function Hero({ onNavigate }: HeroProps) {
           className="text-6xl sm:text-a7xl md:text-8xl font-bold mb-6 text-large text-red-600"
           data-text="EUCRASIA 2026"
         >
-          EUCRASIAA 2026
+          EUCRASIA 2026
         </h1>
 
         <p className="text-2xl sm:text-3xl md:text-4xl text-red-500 mb-4 animate-flicker">
